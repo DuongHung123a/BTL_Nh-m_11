4be95c2180d9dd1d8bb5ea8a9a99aa9333c873ed
letienbao69 <151911705+letienbao69@users.noreply.github.com>
2024-12-07 19:11:32 +0700

    Update login.php

--- BTL_TH2_11/views/admin/login.php
+++ BTL_TH2_11/views/admin/login.php
@@ -11,3 +11,8 @@
         <?php if (isset($error)): ?>
             <div class="alert alert-danger"><?= $error ?></div>
         <?php endif; ?>
+<form method="POST" action="index.php?controller=admin&action=login">
+            <div class="mb-3">
+                <label for="username" class="form-label">Tên đăng nhập</label>
+                <input type="text" name="username" class="form-control" id="username" required>
+            </div>

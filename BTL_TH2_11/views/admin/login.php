07c51846d8ccfe55a2a15a6975261f87a08d2540
letienbao69 <151911705+letienbao69@users.noreply.github.com>
2024-12-07 19:11:02 +0700

    Update login.php

--- BTL_TH2_11/views/admin/login.php
+++ BTL_TH2_11/views/admin/login.php
@@ -1,3 +1,8 @@
 <!DOCTYPE html>
 <html lang="vi">
 <head>
+  <meta charset="UTF-8">
+    <title>Đăng nhập</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>  

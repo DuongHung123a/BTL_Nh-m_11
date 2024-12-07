dad77b1b65f2b44fcf99e1acfdb6a277597c489b
DuongHung123a <144602103+DuongHung123a@users.noreply.github.com>
2024-12-07 18:34:11 +0700

    Update CategoryCotroller.php

--- BTL_TH2_11/controllers/CategoryCotroller.php
+++ BTL_TH2_11/controllers/CategoryCotroller.php
@@ -17,5 +17,25 @@ class CategoryController {
         }
         include 'views/admin/category/add.php';
     }
+        public function edit() {
+        $id = $_GET['id'];
+        $category = Category::getCategoriesById($id);
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $name = $_POST['name'];
+            if (Category::update($id, $name)) {
+                header('Location: index.php?controller=category&action=index');
+                exit;
+            }
+        }
+        include 'views/admin/category/edit.php';
+    }
+
+    public function delete() {
+        $id = $_GET['id'];
+        if (Category::delete($id)) {
+            header('Location: index.php?controller=category&action=index');
+            exit;
+        }
+    }
 
 }

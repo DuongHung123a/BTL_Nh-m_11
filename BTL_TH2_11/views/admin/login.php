0e868cb94722e6ad59fd725ea490edbd590978bc
letienbao69 <151911705+letienbao69@users.noreply.github.com>
2024-12-07 19:11:18 +0700

    Update login.php

--- BTL_TH2_11/views/admin/login.php
+++ BTL_TH2_11/views/admin/login.php
@@ -6,3 +6,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>  
+   <div class="container mt-5">
+        <h2 class="text-center">Đăng nhập quản trị viên</h2>
+        <?php if (isset($error)): ?>
+            <div class="alert alert-danger"><?= $error ?></div>
+        <?php endif; ?>

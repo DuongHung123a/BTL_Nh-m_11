5fe2810ddd02e2877336dc3558cb4b8d6257962e
DuongHung123a <144602103+DuongHung123a@users.noreply.github.com>
2024-12-07 12:10:48 +0700

    Update News.php

--- BTL_TH2_11/models/News.php
+++ BTL_TH2_11/models/News.php
@@ -20,7 +20,14 @@ class News
         return $stmt->fetch();
     }
 
-    public static function add($title, $content, $image, $category_id) {
+    public static function search($keyword) {
+        $db = connDB::getConnection();
+        $stmt = $db->prepare("SELECT news.*, categories.name AS category_name FROM news JOIN categories ON news.category_id = categories.id WHERE news.title LIKE ? OR news.content LIKE ?");
+        $keyword = '%' . $keyword . '%';
+        $stmt->execute([$keyword, $keyword]);
+        return $stmt->fetchAll();
+    }
+        public static function add($title, $content, $image, $category_id) {
         $db = connDB::getConnection();
         $stmt = $db->prepare("INSERT INTO news (title, content, image, category_id) VALUES (?, ?, ?, ?)");
         return $stmt->execute([$title, $content, $image, $category_id]);
@@ -36,14 +43,6 @@ class News
         $db = connDB::getConnection();
         $stmt = $db->prepare("DELETE FROM news WHERE id = ?");
         return $stmt->execute([$id]);
-    }
-
-    public static function search($keyword) {
-        $db = connDB::getConnection();
-        $stmt = $db->prepare("SELECT news.*, categories.name AS category_name FROM news JOIN categories ON news.category_id = categories.id WHERE news.title LIKE ? OR news.content LIKE ?");
-        $keyword = '%' . $keyword . '%';
-        $stmt->execute([$keyword, $keyword]);
-        return $stmt->fetchAll();
     }
 }
 ?>

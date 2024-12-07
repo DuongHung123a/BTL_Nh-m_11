bd91cd73bdf8abf910c74a5a9df691527f6eff22
DuongHung123a <144602103+DuongHung123a@users.noreply.github.com>
2024-12-07 18:22:19 +0700

    Update AdminController.php

--- BTL_TH2_11/controllers/AdminController.php
+++ BTL_TH2_11/controllers/AdminController.php
@@ -39,5 +39,30 @@ class AdminController {
         $news = News::getById($id);
         include 'views/admin/news/edit.php';
     }
+     public function updateNews() {
+        $this->requireLogin();
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $id = $_GET['id'];
+            $title = $_POST['title'];
+            $content = $_POST['content'];
+            $image = $_POST['image'];
+            $category_id = $_POST['category_id'];
+            if (News::update($id, $title, $content, $image, $category_id)) {
+                echo "<script>alert('Sửa tin tức thành công');</script>";
+                header('Location: index.php?controller=admin&action=dashboard');
+                exit;
+            }
+        }
+    }
+
+    public function deleteNews() {
+        $this->requireLogin();
+        $id = $_GET['id'];
+        if (News::delete($id)) {
+            echo "<script>alert('Xóa tin tức thành công');</script>";
+            header('Location: index.php?controller=admin&action=dashboard');
+            exit;
+        }
+    }
 
 }

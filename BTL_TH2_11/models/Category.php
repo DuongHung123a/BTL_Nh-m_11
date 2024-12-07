994abe0a03b06bc420c40e0d8a6f9bb71ada2464
DuongHung123a <144602103+DuongHung123a@users.noreply.github.com>
2024-12-07 11:52:00 +0700

    Update Category.php

---
+++ BTL_TH2_11/models/Category.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'config/connDB.php';
+class Category {
+    public static function getAllCategories() {
+        $db = connDB::getConnection();
+        $stmt = $db->query("SELECT * FROM categories");
+        return $stmt->fetchAll();
+    }
+
+    public static function getCategoriesById($id) {
+        $db = connDB::getConnection();
+        $stmt = $db->prepare("SELECT * FROM categories WHERE id = ?");
+        $stmt->execute([$id]);
+        return $stmt->fetch();
+    }
+      public static function add($name) {
+        $db = connDB::getConnection();
+        $stmt = $db->prepare("INSERT INTO categories (name) VALUES (?)");
+        return $stmt->execute([$name]);
+    }
+
+}
+?>

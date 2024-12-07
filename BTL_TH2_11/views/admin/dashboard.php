01d60a0fa6cbcd5db9d7b5e03c31f950095bec75
letienbao69 <151911705+letienbao69@users.noreply.github.com>
2024-12-07 19:08:52 +0700

    Update dashboard.php

--- BTL_TH2_11/views/admin/dashboard.php
+++ BTL_TH2_11/views/admin/dashboard.php
@@ -3,3 +3,13 @@
     <h2>Quản lý tin tức</h2>
     <a href="index.php?controller=admin&action=addNews" class="btn btn-primary mb-3">Thêm tin tức mới</a>
     <table class="table table-bordered">
+   <thead>
+            <tr>
+                <th>ID</th>
+                <th>Tiêu đề</th>
+                <th>Hình ảnh</th>
+                <th>Sửa</th>
+                <th>Xóa</th>
+            </tr>
+        </thead>
+        <tbody>
